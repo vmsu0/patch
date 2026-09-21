cd56b0479d4bee5d2f0c129a939b143bb14fc7a7
vmsu <18164452+vmsu0@users.noreply.github.com>
2026-09-21 14:46:13 +0800

    Fix author credit and clean up ps.sh
    Updated author credit and removed commented-out code for clarity.

--- resukisu/ps.sh
+++ resukisu/ps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Patches author: backslashxx @ Github
+# Patches author: backslashxx @ GitHub
 # Shell authon: JackA1ltman <cs2dtzq@163.com>
 # Tested kernel versions: 5.4, 4.19, 4.14, 4.9, 4.4, 3.18
 # 20250309
@@ -17,7 +17,6 @@ patch_files=(
     security/selinux/ss/services.c
     kernel/reboot.c
     kernel/sys.c
-    include/linux/seccomp.h
 )
 
 PATCH_LEVEL="2.2"
@@ -407,28 +406,6 @@ extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void 
             fi
         else
             echo "[-] KernelSU have no ksu_handle_setresuid, Skipped."
-        fi
-
-        echo "======================================"
-        ;;
-
-    # include/ changes
-    ## linux/seccomp.h
-    include/linux/seccomp.h)
-        echo "======================================"
-
-        if grep -q "filter_count" "include/linux/seccomp.h" >/dev/null 2>&1; then
-            echo "[-] Detected filter_count in kernel, Skipped."
-        else
-            sed -i '/#include <linux\/thread_info.h>/a\#include <linux\/atomic.h>' include/linux/seccomp.h
-            sed -i '/struct seccomp_filter \*filter;/i\ \tatomic_t filter_count;' include/linux/seccomp.h
-
-            if grep -q "filter_count" "include/linux/seccomp.h"; then
-                echo "[+] include/linux/seccomp.h Patched!"
-                echo "[+] Count: $(grep -c "filter_count" "include/linux/seccomp.h")"
-            else
-                echo "[-] include/linux/seccomp.h patch failed for unknown reasons, please provide feedback in time."
-            fi
         fi
 
         echo "======================================"
